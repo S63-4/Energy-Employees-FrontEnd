f4cf83384a7fa0b7dd88c0fcf1b7009a3fd1fdf8
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-04-09 10:12:24 +0200

    changepass

--- energy-grid-employee/src/app/changepassword/changepassword.component.ts
+++ energy-grid-employee/src/app/changepassword/changepassword.component.ts
@@ -6,12 +6,23 @@ import { AppConfig } from "../app.config";
   selector: "app-changepassword",
   templateUrl: "./changepassword.component.html",
   styleUrls: ["./changepassword.component.scss"],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ChangePasswordComponent implements OnInit {
-  
-  constructor(private http: HttpClient) {
-  }
+  current: string;
+  newpass1: string;
+  newpass2: string;
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
+
+  changepass():void {
+    if (this.newpass1 === this.newpass2) {
+      var url = `${AppConfig.ApiBaseURL}${AppConfig.ApiUrls.CHANGEPASS}`;
+      this.http.post(url, {
+        oldPass : this.current,
+        newPass: this.newpass1
+      })
+    }
+  };
 }
