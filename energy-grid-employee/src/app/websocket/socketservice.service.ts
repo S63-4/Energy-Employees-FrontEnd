c8296c80541d0682228e88324ab75dbe282d487d
Lotte Diesveld <l.diesveld@student.fontys.nl>
2020-04-14 10:21:52 +0200

    rabbitmq basic

--- energy-grid-employee/src/app/websocket/socketservice.service.ts
+++ energy-grid-employee/src/app/websocket/socketservice.service.ts
@@ -13,6 +13,11 @@ export class SocketService {
 
   private subject: Rx.Subject<MessageEvent>;
 
+  private wsbroker: string;
+  private wsport: number;
+  private client: mqtt.MqttClient;
+  private options: any;
+
   public connect(url): Rx.Subject<MessageEvent> {
 
 
@@ -42,5 +47,35 @@ export class SocketService {
       }
     };
     return Rx.Subject.create(observer, observable);
+  }
+
+  private test(){
+    this.wsbroker = location.hostname;  // mqtt websocket enabled broker
+    this.wsport = 15675; // port for above
+    this.client = new mqtt.Client(this.wsbroker, this.wsport);
+    this.client.onConnectionLost = function (responseObject) {
+      console.log("CONNECTION LOST - " + responseObject.toString());
+    };
+    this.client.onMessageArrived = function (message) {
+      console.log("RECEIVE ON " + message.destinationName + " PAYLOAD " + message.payloadString);
+      console.log(message.toString());
+    };
+
+    this.options = {
+      timeout: 3,
+      keepAliveInterval: 30,
+      onSuccess: function () {
+        console.log("CONNECTION SUCCESS");
+        this.client.subscribe('/topic/test', {qos: 1});
+      },
+      onFailure: function (message) {
+        console.log("CONNECTION FAILURE - " + message.errorMessage);
+      }
+    };
+    if (location.protocol == "https:") {
+      this.options.useSSL = true;
+    }
+    console.log("CONNECT TO " + this.wsbroker + ":" + this.wsport);
+    this.client.connect(this.options);
   }
 }
