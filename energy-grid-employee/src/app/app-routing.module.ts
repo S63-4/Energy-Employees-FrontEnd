c461b52b32e61431992e665c9864f18311153f93
tcskrijnen <tim.krijnen9@gmail.com>
2020-05-06 14:51:33 +0200

    basic UI and app routing

--- energy-grid-employee/src/app/app-routing.module.ts
+++ energy-grid-employee/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { ChangePasswordComponent } from './changepassword/changepassword.component';
 import { CustomermanagementComponent} from "./customermanagement/customermanagement.component";
 import { NewcustomerComponent } from "./customermanagement/newcustomer/newcustomer.component";
+import { SearchcustomerComponent} from "./customermanagement/searchcustomer/searchcustomer.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'changepass', component: ChangePasswordComponent},
   { path: 'customermanagement', component: CustomermanagementComponent },
-  { path: 'customermanagement/newcustomer', component: NewcustomerComponent }
+  { path: 'customermanagement/newcustomer', component: NewcustomerComponent },
+  { path: 'customermanagement/searchcustomer', component: SearchcustomerComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
