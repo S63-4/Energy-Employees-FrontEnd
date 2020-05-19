e88f6ad5af2d1faf98051a59cbfb218c3c414d15
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-05-19 16:04:16 +0200

    uris

--- energy-grid-employee/src/app/app.config.ts
+++ energy-grid-employee/src/app/app.config.ts
@@ -1,22 +1,21 @@
-
-
 export class AppConfig {
-  static ApiBaseURL = "http://localhost:8762";
+  // static WebSocketBaseUrl = "http://localhost:9060/";
+  static WebSocketBaseUrl = "http://10.93.1.110:9060/";
 
- // static WebSocketBaseUrl = "http://localhost:9060/";
-  static WebSocketBaseUrl = "http://10.93.1.110:9060/";  
+  // static ApiBaseURL = "http://localhost:8762";
+  static ApiBaseURL = "http://35.189.86.8";
   static ApiUrls = {
     LOGIN: "/login",
     CHANGEPASS: "/user/user/changepass",
     AllCUSTOMERS: "/user/customer/profiles",
     NEWCUSTOMER: "/user/customer/new",
     SEARCHCUSTOMER: "/user/customer/getByCustomerCode",
-    DELETECUSTOMER: "/user/customer/delete"
+    DELETECUSTOMER: "/user/customer/delete",
   };
   static LocalStorageKeys = {
     TOKEN: "token",
   };
-/*  static LocalStorageMessage = {
+  /*  static LocalStorageMessage = {
     MESSAGE: Rcv_message
   };*/
 }
