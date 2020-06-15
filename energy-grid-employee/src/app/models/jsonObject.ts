04d220192f008a929c07fb65add889c4f48453a1
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-06-15 14:45:48 +0200

    sharedjson

--- energy-grid-employee/src/app/models/jsonObject.ts
+++ energy-grid-employee/src/app/models/jsonObject.ts
@@ -1,45 +1,28 @@
-import DateTimeFormat = Intl.DateTimeFormat;
+export interface SharedJSON {
+  dateTime: Date;
+  region: string;
+  consumption: number;
+  production: number;
 
-export class JsonObject {
-  private _date: string;
-  private _region: string;
-  private _consumption: number;
-  private _production: number;
+  new (): SharedJSON;
 
+  new (
+    dateTime: Date,
+    consumption: number,
+    production: number
+  ): SharedJSON;
 
-  constructor() {
-  }
+  getDate(): Date;
 
+  setDate(dateTime: Date): void;
 
-  get date(): string {
-    return this._date;
-  }
+  getRegion(): string;
 
-  set date(value: string) {
-    this._date = value;
-  }
+  getConsumption(): number;
 
-  get region(): string {
-    return this._region;
-  }
+  setConsumption(consumption: number): void;
 
-  set region(value: string) {
-    this._region = value;
-  }
+  getProduction(): number;
 
-  get consumption(): number {
-    return this._consumption;
-  }
-
-  set consumption(value: number) {
-    this._consumption = value;
-  }
-
-  get production(): number {
-    return this._production;
-  }
-
-  set production(value: number) {
-    this._production = value;
-  }
+  setProduction(production: number): void;
 }
