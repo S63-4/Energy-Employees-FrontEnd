7dc53bd3da3b0ebaebf05466c64d89d23b01621c
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-06-18 16:08:16 +0200

    staylogin

--- energy-grid-employee/src/app/REST/authentication.service.ts
+++ energy-grid-employee/src/app/REST/authentication.service.ts
@@ -1,17 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable, of, BehaviorSubject, from } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
-import { AppConfig } from '../app.config';
-import { CookieService } from 'ngx-cookie-service';
-import { user } from '../models/user';
-import { customer } from '../models/customer';
+import { Injectable } from "@angular/core";
+import { HttpHeaders, HttpClient, HttpResponse } from "@angular/common/http";
+import { Observable, of, BehaviorSubject, from } from "rxjs";
+import { map, catchError } from "rxjs/operators";
+import { AppConfig } from "../app.config";
+import { CookieService } from "ngx-cookie-service";
+import { user } from "../models/user";
+import { customer } from "../models/customer";
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class AuthenticationService {
   private isLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject(false);
-  constructor(private http: HttpClient, private cookieService: CookieService) {}
+  constructor(private http: HttpClient, private cookieService: CookieService) {
+    var token = this.cookieService.get("authorization-key");
+    this.isLoggedIn.next(!!token);
+  }
 
   /** GET login codes from the server */
   getLogin(clientnr: string, password: string) {
@@ -20,19 +23,19 @@ export class AuthenticationService {
       .post<HttpResponse<any>>(
         URL,
         { username: clientnr, password },
-        { observe: 'response' }
+        { observe: "response" }
       )
       .subscribe(
         (response) => {
-          let token = response.headers.get('Authorization');
+          let token = response.headers.get("Authorization");
           if (token) {
             // localStorage.setItem(AppConfig.LocalStorageKeys.TOKEN, token);
-            this.cookieService.set('authorization-key', token);
+            this.cookieService.set("authorization-key", token);
           }
           this.isLoggedIn.next(!!token);
         },
         (error) => {
-          alert('Email and Password combination is incorrect!');
+          alert("Email and Password combination is incorrect!");
         }
       );
   }
@@ -45,7 +48,7 @@ export class AuthenticationService {
   // * Get authorization token */
   public getAuthorizationToken(): string {
     // return localStorage.getItem(AppConfig.LocalStorageKeys.TOKEN);
-    return this.cookieService.get('authorization-key');
+    return this.cookieService.get("authorization-key");
   }
 
   // * Logout */
@@ -55,18 +58,18 @@ export class AuthenticationService {
 
   /** POST: add a new user to the server */
   postRegister(user: user): Observable<any> {
-    const serverURL = AppConfig.ApiBaseURL + 'UserController/registration';
+    const serverURL = AppConfig.ApiBaseURL + "UserController/registration";
     return this.http.post<user>(serverURL, user).pipe(
       map((result) => (result as unknown) as string),
-      catchError(this.handleError<any>('postRegistert'))
+      catchError(this.handleError<any>("postRegistert"))
     );
   }
 
   postNewCustomer(customer: customer): Observable<any> {
-    const serverURL = AppConfig.ApiBaseURL + AppConfig.ApiUrls.NEWCUSTOMER
+    const serverURL = AppConfig.ApiBaseURL + AppConfig.ApiUrls.NEWCUSTOMER;
     return this.http.post<customer>(serverURL, customer).pipe(
       map((result) => (result as unknown) as string),
-      catchError(this.handleError<any>('postNewCustomer'))
+      catchError(this.handleError<any>("postNewCustomer"))
     );
   }
 
@@ -76,7 +79,7 @@ export class AuthenticationService {
    * @param operation - name of the operation that failed
    * @param result - optional value to return as the observable result
    */
-  private handleError<T>(operation = 'operation', result?: T) {
+  private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
       // Let the user know how to register properly
       console.log(error);
--- energy-grid-employee/src/app/app.component.ts
+++ energy-grid-employee/src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
     private router: Router
   ) {
     this.subscribeEvents();
+    
   }
 
   subscribeEvents(): void {
