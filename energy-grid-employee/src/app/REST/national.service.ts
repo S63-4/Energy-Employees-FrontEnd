14fb7374754bf6b4858826a991a01ad4d783d4fe
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-06-22 13:05:54 +0200

    fix national dash

--- energy-grid-employee/src/app/REST/national.service.ts
+++ energy-grid-employee/src/app/REST/national.service.ts
@@ -1,24 +1,23 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {CookieService} from "ngx-cookie-service";
-import {AppConfig} from "../app.config";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { CookieService } from "ngx-cookie-service";
+import { AppConfig } from "../app.config";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NationalService {
-
   constructor(private http: HttpClient) {}
 
   getRegional(region: String) {
     var URL;
-    if (region === "Noord-Brabant"){
+    if (region === "Noord-Brabant") {
       URL = `https://staging.external.powerprice.marstan.net/status/getStatus`;
-    } else if (region === "Zuid-Holland"){
-      URL = `http://fsenergyservice.daphneprojects.online/energie/nh/2020/6/18`;
-    } else if (region === "Friesland"){
+    } else if (region === "Zuid-Holland") {
+      URL = `http://fsenergyservice.daphneprojects.online/energie/nh/2020/6/${new Date().getDate()}`;
+    } else if (region === "Friesland") {
       URL = ``;
-    } else if (region === "Zeeland"){
+    } else if (region === "Zeeland") {
       URL = `${AppConfig.ApiBaseURL}/rest-forwarder/regional`;
     }
 
